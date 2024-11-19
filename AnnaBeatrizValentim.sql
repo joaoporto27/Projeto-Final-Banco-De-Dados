9ff46588f05e8993130fcc975d83cca88f804332
Anna Beatriz <annavalentim.tds1.senai@gmail.com>
2024-11-19 08:52:17 -0300

    add inserts

--- AnnaBeatrizValentim.sql
+++ AnnaBeatrizValentim.sql
@@ -4,3 +4,10 @@ CREATE TABLE sobrenos (
     descricao TEXT NOT NULL,
     idade INTEGER NOT NULL
 )
+
+INSERT INTO sobrenos (nomeParticipante, descricao, idade) VALUES 
+('Ana Julia Demattei', 'Desenvolvedora', 16),
+('Anna Beatriz Ribeiro Valentim', 'Desenvolvedora', 16),
+('Bernardo Gabriel de Moraes Marques', 'Desenvolvedor', 16),
+('Isabella Borin de Moraes Rosa', 'Scrum Master', 16),
+('João Vitor Porto Sales', 'Product Owner', 16);
