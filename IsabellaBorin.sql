e93200b9210dcc691d11d1ab35da08d5ad357fd1
Isabella Borin <158210136+isab5@users.noreply.github.com>
2024-11-26 17:11:35 -0300

    feat: create tables from "simulado"

--- IsabellaBorin.sql
+++ IsabellaBorin.sql
@@ -13,3 +13,62 @@ VALUES
 ('Docência', 'https://site-filosofia.vercel.app/img/professorcarreiras.png', 'Engloba as diversas possibilidades da educação de jovens e adultos, nas redes pública e privada.'),
 ('Pesquisa e carreira acadêmica', 'https://site-filosofia.vercel.app/img/academico.png', 'O pesquisadores acadêmicos na área de Filosofia desenvolvem pesquisas que ampliam os conhecimentos filosóficos e científicos.'),
 ('Colunista e crítico em veículos de comunicação.', 'https://site-filosofia.vercel.app/img/m%C3%ADdias.png', 'No campo da filosofia um desses trabalhos é como colunista em veículos de comunicação, como jornais, revistas, rádio, TV e websites, explorando as mais diversas temáticas.');
+
+<-- Conteúdo feito junto com  a Anna Beatriz Valentim --->
+
+CREATE TABLE simulados (
+    id_simulado SERIAL PRIMARY KEY,
+    nome_simulado VARCHAR(250) NOT NULL,
+    imagem VARCHAR(255) NOT NULL
+);
+
+INSERT INTO simulados (nome_simulado, imagem) VALUES
+('Filosofia Antiga', 'https://site-filosofia.vercel.app/img/simulado01.png'),
+('Filosofia Moderna', 'https://site-filosofia.vercel.app/img/simulado03.png'),
+('Filosofia Medieval', 'https://site-filosofia.vercel.app/img/simulado02.png'),
+('Filosofia Contemporânea', 'https://site-filosofia.vercel.app/img/simulado04.png');
+
+CREATE TABLE filosofiaAntiga(
+    id_questao SERIAL PRIMARY KEY,
+    id_simulado INTEGER NOT NULL,
+    questoes INTEGER NOT NULL,
+    descricao TEXT NOT NULL,
+    respostas_simulado VARCHAR(1) NOT NULL,
+    CONSTRAINT fk_simulado FOREIGN KEY (id_simulado) REFERENCES simulados(id)
+);
+
+CREATE TABLE filosofiaMedieval (
+    id_questao SERIAL PRIMARY KEY,
+    id_simulado INTEGER NOT NULL,
+    questoes INTEGER NOT NULL,
+    descricao TEXT NOT NULL,
+    respostas_simulado VARCHAR(1) NOT NULL,
+    CONSTRAINT fk_simulado FOREIGN KEY (id_simulado) REFERENCES simulados(id)
+);
+
+CREATE TABLE filosofiaMedieval (
+    id_questao SERIAL PRIMARY KEY,
+    id_simulado INTEGER NOT NULL,
+    questoes INTEGER NOT NULL,
+    descricao TEXT NOT NULL,
+    respostas_simulado VARCHAR(1) NOT NULL,
+    CONSTRAINT fk_simulado FOREIGN KEY (id_simulado) REFERENCES simulados(id)
+);
+
+CREATE TABLE filosofiaModerna (
+    id_questao SERIAL PRIMARY KEY,
+    id_simulado INTEGER NOT NULL,
+    questoes INTEGER NOT NULL,
+    descricao TEXT NOT NULL,
+    respostas_simulado VARCHAR(1) NOT NULL,
+    CONSTRAINT fk_simulado FOREIGN KEY (id_simulado) REFERENCES simulados(id)
+);
+
+CREATE TABLE filosofiaContemporanea (
+    id_questao SERIAL PRIMARY KEY,
+    id_simulado INTEGER NOT NULL,
+    questoes INTEGER NOT NULL,
+    descricao TEXT NOT NULL,
+    respostas_simulado VARCHAR(1) NOT NULL,
+    CONSTRAINT fk_simulado FOREIGN KEY (id_simulado) REFERENCES simulados(id)
+);
