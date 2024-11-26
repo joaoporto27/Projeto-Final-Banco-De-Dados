de302085514cdf12e43f424e1503887f8de8b584
Anna Beatriz <annavalentim.tds1.senai@gmail.com>
2024-11-26 15:29:13 -0300

    fix: adjustments to the table, links and inserts

--- AnnaBeatrizValentim.sql
+++ AnnaBeatrizValentim.sql
@@ -1,14 +1,14 @@
 CREATE TABLE sobrenos (
     id_sobrenos SERIAL PRIMARY KEY,
-    foto_participante VARCHAR(255) NOT NULL,
+    fotoParticipante VARCHAR(255) NOT NULL,
     nomeParticipante VARCHAR(100) NOT NULL,
     descricao TEXT NOT NULL,
     idade INTEGER NOT NULL
 )
 
-INSERT INTO sobrenos (nomeParticipante, descricao, idade, foto_participante) VALUES 
-('Ana Julia Demattei', 'Desenvolvedora', 16, '../img/AnaJulia.png'),
-('Anna Beatriz Ribeiro Valentim', 'Desenvolvedora', 16, '../img/AnnaValentim.png'),
-('Bernardo Gabriel de Moraes Marques', 'Desenvolvedor', 16, '../img/Bernardo.png'),
-('Isabella Borin de Moraes Rosa', 'Scrum Master', 16,'../img/Isabella.png'),
-('João Vitor Porto Sales', 'Product Owner', 16, '../img/JoãoPortoSales.png');
+INSERT INTO sobrenos (nomeParticipante, descricao, idade, fotoParticipante) VALUES 
+('Ana Julia Demattei', 'Desenvolvedora', 16, 'https://site-filosofia.vercel.app/img/AnaJulia.png'),
+('Anna Beatriz Ribeiro Valentim', 'Desenvolvedora', 16, 'https://site-filosofia.vercel.app/img/AnnaValentim.png'),
+('Bernardo Gabriel de Moraes Marques', 'Desenvolvedor', 16, 'https://site-filosofia.vercel.app/img/Bernardo.png'),
+('Isabella Borin de Moraes Rosa', 'Scrum Master', 16,'https://site-filosofia.vercel.app/img/Isabella.png'),
+('João Vitor Porto Sales', 'Product Owner', 16, 'https://site-filosofia.vercel.app/img/Jo%C3%A3oPortoSales.png');
