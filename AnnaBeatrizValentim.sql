e43dd2a7183e2d8af934fa720215ce6c9f6c2611
Anna Beatriz <annavalentim.tds1.senai@gmail.com>
2024-11-26 15:24:32 -0300

    fix: adjustments to the table and inserts

--- AnnaBeatrizValentim.sql
+++ AnnaBeatrizValentim.sql
@@ -1,13 +1,14 @@
 CREATE TABLE sobrenos (
     id_sobrenos SERIAL PRIMARY KEY,
+    foto_participante VARCHAR(255) NOT NULL,
     nomeParticipante VARCHAR(100) NOT NULL,
     descricao TEXT NOT NULL,
     idade INTEGER NOT NULL
 )
 
-INSERT INTO sobrenos (nomeParticipante, descricao, idade) VALUES 
-('Ana Julia Demattei', 'Desenvolvedora', 16),
-('Anna Beatriz Ribeiro Valentim', 'Desenvolvedora', 16),
-('Bernardo Gabriel de Moraes Marques', 'Desenvolvedor', 16),
-('Isabella Borin de Moraes Rosa', 'Scrum Master', 16),
-('João Vitor Porto Sales', 'Product Owner', 16);
+INSERT INTO sobrenos (nomeParticipante, descricao, idade, foto_participante) VALUES 
+('Ana Julia Demattei', 'Desenvolvedora', 16, '../img/AnaJulia.png'),
+('Anna Beatriz Ribeiro Valentim', 'Desenvolvedora', 16, '../img/AnnaValentim.png'),
+('Bernardo Gabriel de Moraes Marques', 'Desenvolvedor', 16, '../img/Bernardo.png'),
+('Isabella Borin de Moraes Rosa', 'Scrum Master', 16,'../img/Isabella.png'),
+('João Vitor Porto Sales', 'Product Owner', 16, '../img/JoãoPortoSales.png');
